f5dcbc8d96b699cb49a2482985b7f5b6c288ed10
Isaaczhong7 <bingxiangzhong@aol.com>
2024-09-27 16:07:58 -0400

    add new file and code for MinMaxCalculation

---
+++ MinMaxCalculation.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class MinMaxCalculation {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements:");
+        int n = scanner.nextInt();  
+
+        if (n <= 0) {
+            System.out.println("Please enter a positive number of elements.");
+            return;
+        }
+
+        int[] numbers = new int[n];
+
+        System.out.println("Enter the numbers:");
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        // Calculate min and max
+        int min = findMin(numbers);
+        int max = findMax(numbers);
+
+        System.out.println("Minimum number: " + min);
+        System.out.println("Maximum number: " + max);
+
+        scanner.close();
+    }
+
+    public static int findMin(int[] nums) {
+        //TODO
+    }
+
+    public static int findMax(int[] nums) {
+        //TODO
+    }
+}
